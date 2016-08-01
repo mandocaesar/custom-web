293e4515b7b840641c1472f1576bce3bba2b2af7
Armand Caesar <armand.caesar@gmail.com>
2016-08-01 09:05:24 +0700

    init

---
+++ assets/js/app.js
@@ -0,0 +1,56 @@
+var LocsB = [
+    {
+        lat: 52.1,
+        lon: 11.3,
+        title: 'Title A2',
+        html: [
+            '<h3>Content A2</h3>',
+            '<p>Lorem Ipsum</p>'
+        ].join(''),
+        zoom: 8
+    },
+    {
+        lat: 51.2,
+        lon:   22.2,
+        title: 'Title B2',
+        html: [
+            '<h3>Content B2</h3>',
+            '<p>Lorem Ipsum</p>'
+        ].join(''),
+        zoom: 8
+    },
+    {
+        lat: 49.4,
+        lon: 35.9,
+        title: 'Title C2',
+        html: [
+            '<h3>Content C2</h3>',
+            '<p>Lorem Ipsum</p>'            
+        ].join(''),
+        zoom: 4
+    },
+    {
+        lat: 47.8,
+        lon: 15.6,
+        title: 'Title D2',
+        html: [
+            '<h3>Content D2</h3>',
+            '<p>Lorem Ipsum</p>'
+        ].join(''),
+        zoom: 6
+    }
+];
+
+
+new Maplace({
+    locations: LocsB,
+    map_div: '#gmap-tabs',
+    controls_div: '#controls-tabs',
+    controls_type: 'list',
+    controls_on_map: false,
+    show_infowindow: false,
+    start: 1,
+    afterShow: function(index, location, marker) {
+        $('#info').html(location.html);
+    }
+}).Load();
